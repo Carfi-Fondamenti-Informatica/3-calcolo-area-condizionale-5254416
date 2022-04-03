2e2f7e485884e7908119691ed060221f7ab82d3f
5254416 <100807837+5254416@users.noreply.github.com>
2022-04-03 17:34:57 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,6 +1,23 @@
 #include <iostream>
 using namespace std;
 
-int main() {
-   return 0;
+int main(){
+  float a, b, area;
+  int op;
+  std::cout<<"Inserire due numeri reali e scegliere l'opzione.\n";
+  cin >> a >> b >> op;
+  if (op==0) {
+      area = (a*b)/2;
+      std::cout << area;
+  } else if (op==1) {
+      area = a*a;
+      std::cout << area;
+  } else if (op==2){
+      area = a*b;
+      std::cout << area;
+  } else {
+      std::cout << "Opzione non disponibile";
+  }
+  return 0;
+
 }
